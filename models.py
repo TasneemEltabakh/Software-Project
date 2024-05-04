6b8812098b4f950c8b4621e00b598ede5cd5b846
nadasoudi <s-nada.soudi@zewailcity.edu.eg>
2024-05-04 17:34:16 +0300

    update_db

--- models.py
+++ models.py
@@ -66,4 +66,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message %r>' % self.id
-# 
+
+
+class ContactMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<ContactMessage {self.name}>'
